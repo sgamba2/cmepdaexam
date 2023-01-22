d110c0dc38424ccba053d524dd0f5f20aa13dd78
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-01-22 21:36:28 +0100

    Add files via upload

---
+++ afb.cpp
@@ -0,0 +1,106 @@
+#include "utilities.h"
+#include <ROOT/RDataFrame.hxx>
+#include <ROOT/RVec.hxx>
+#include <Math/Vector4Dfwd.h>
+#include <Math/Vector4D.h>
+#include <TCanvas.h>
+#include <TH1D.h>
+#include <TLatex.h>
+#include <TStyle.h>
+#include <TROOT.h>
+#include <TLegend.h>
+
+void afb(){
+
+    /*
+    Parameters
+    ----------
+    It doesn't take any parameter
+
+    Returns
+    ---------
+    It is a void function and it generates different 
+    plots (based on rapidity conditions) of the forward backward asimmetry.
+    All the plots are saved in images/afb/file.pdf.
+    */
+
+    // Enable multi-threading
+    // The default here is set to a single thread. You can choose the number of threads based on your system.
+    ROOT::EnableImplicitMT();
+
+    ROOT::RDataFrame df_MC("Events_new", "Events.root");
+
+    auto df_2mu_MC = allquantities(df_MC);
+    
+    df_2mu_MC=df_2mu_MC.Define("wd","0.5*pow(costheta,2)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),3)");
+
+    df_2mu_MC=df_2mu_MC.Define("wn","0.5*fabs(costheta)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),2)");
+
+    auto df_2mu_MC1= df_2mu_MC.Filter("fabs(y)<0.4 ", "y1");
+    auto df_2mu_MC2= df_2mu_MC.Filter("fabs(y)<1.2 && fabs(y)>0.8", "y2");
+    auto df_2mu_MC3= df_2mu_MC.Filter("fabs(y)<2.0 && fabs(y)>1.6", "y3");
+  
+    auto df_cm1=df_2mu_MC1.Filter("costheta<0", "backward");
+    auto df_cp1=df_2mu_MC1.Filter("costheta>=0", "forward");
+    auto df_cm2=df_2mu_MC2.Filter("costheta<0", "backward");
+    auto df_cp2=df_2mu_MC2.Filter("costheta>=0", "forward");
+    auto df_cm3=df_2mu_MC3.Filter("costheta<0", "backward");
+    auto df_cp3=df_2mu_MC3.Filter("costheta>=0", "forward");
+
+    //first condition of rapidity
+    auto histDf1 = df_cp1.Histo2D({"cp,Df1", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wd");
+    auto histNf1 = df_cp1.Histo2D({"cp,Nf1", "cp,Nf", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wn");
+    auto histDb1 = df_cm1.Histo2D({"cm,Db1", "cm,Db", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wd");
+    auto histNb1 = df_cm1.Histo2D({"cm,Nb1", "cm,Nb", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wn");
+
+    histNf1->Add(histNb1.GetPtr(),-1.0);
+    histDf1->Add(histDb1.GetPtr(),+1.0);
+    histNf1->Divide(histDf1.GetPtr());
+    histNf1->Scale(0.375);
+
+    auto c1 = new TCanvas("c1", "c1", 1000, 700);
+    histNf1->GetXaxis()->SetTitle("m_{#mu#mu}");
+    histNf1->GetXaxis()->SetTitleSize(0.04);
+    histNf1->GetYaxis()->SetTitle("MC");
+    histNf1->GetYaxis()->SetTitleSize(0.04);
+    histNf1->SetStats(0);
+    histNf1->DrawClone();
+
+    //second condition of rapidity
+    auto histDf2 = df_cp2.Histo2D({"cp,Df2", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wd");
+    auto histNf2 = df_cp2.Histo2D({"cp,Nf2", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wn");
+    auto histDb2 = df_cm2.Histo2D({"cm,Db2", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wd");
+    auto histNb2 = df_cm2.Histo2D({"cm,Nb2", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wn");
+
+    histNf2->Add(histNb2.GetPtr(),-1.0);
+    histDf2->Add(histDb2.GetPtr(),+1.0);
+    histNf2->Divide(histDf2.GetPtr());
+    histNf2->Scale(0.375);
+
+    auto c2 = new TCanvas("c2", "c2", 1000, 700);
+    histNf2->GetXaxis()->SetTitle("m_{#mu#mu}");
+    histNf2->GetXaxis()->SetTitleSize(0.04);
+    histNf2->GetYaxis()->SetTitle("MC");
+    histNf2->GetYaxis()->SetTitleSize(0.04);
+    histNf2->SetStats(0);
+    histNf2->DrawClone();
+
+    //third condition of rapidity
+    auto histDf3 = df_cp3.Histo2D({"cp,Df3", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wd");
+    auto histNf3 = df_cp3.Histo2D({"cp,Nf3", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wn");
+    auto histDb3 = df_cm3.Histo2D({"cm,Db3", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wd");
+    auto histNb3 = df_cm3.Histo2D({"cm,Nb3", "", 30, 0.25,300,30,-2.4,2.4},"dimuon_mass","y","wn");
+
+    histNf3->Add(histNb3.GetPtr(),-1.0);
+    histDf3->Add(histDb3.GetPtr(),+1.0);
+    histNf3->Divide(histDf3.GetPtr());
+    histNf3->Scale(0.375);
+
+    auto c3 = new TCanvas("c3", "c3", 1000, 700);
+    histNf3->GetXaxis()->SetTitle("m_{#mu#mu}");
+    histNf3->GetXaxis()->SetTitleSize(0.04);
+    histNf3->GetYaxis()->SetTitle("MC");
+    histNf3->GetYaxis()->SetTitleSize(0.04);
+    histNf3->SetStats(0);
+    histNf3->DrawClone();
+}
---
+++ dimuon_spectrum_Z.cpp
@@ -0,0 +1,175 @@
+#include "utilities.h"
+#include <ROOT/RDataFrame.hxx>
+#include <ROOT/RVec.hxx>
+#include <Math/Vector4Dfwd.h>
+#include <Math/Vector4D.h>
+#include <TCanvas.h>
+#include <TH1D.h>
+#include <TLatex.h>
+#include <TStyle.h>
+#include <TLegend.h>
+
+void dimuon_spectrum_Z(){
+    
+    /*
+    Parameters
+    ----------
+    It doesn't take any parameter
+
+    Returns
+    ---------
+    It is a void function and it generates three different 
+    plots (based on rapidity conditions) of the dimuon invariant mass.
+    All the plots are saved in images/dimuonspectrumZ/file.pdf.
+    */
+
+    // Enable multi-threading
+    // The default here is set to a single thread. You can choose the number of threads based on your system.
+    ROOT::EnableImplicitMT();
+
+    ROOT::RDataFrame df_MC("Events_new", "Events.root");
+    
+    auto df_mass_MC = df_MC.Define("dimuon_mass", computeInvariantMass, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"}); //
+    
+    df_mass_MC=df_mass_MC.Define("y", computey,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+
+    auto df_mass1_MC= df_mass_MC.Filter("fabs(y)<0.4 ", "y1");
+    auto df_mass2_MC= df_mass_MC.Filter("fabs(y)<1.2 && fabs(y)>0.8", "y2");
+    auto df_mass3_MC= df_mass_MC.Filter("fabs(y)<2.0 && fabs(y)>1.6", "y3");
+
+    constexpr int bins = 60; // Number of bins in the histogram
+    constexpr float low = 70.0; // Lower edge of the histogram
+    constexpr float up = 110.0; // Upper edge of the histogram
+    
+    auto hist1 = df_mass1_MC.Histo1D({"hist1", "", bins, low, up}, "dimuon_mass");
+    // Request cut-flow report
+    auto report1 = df_mass1_MC.Report();
+
+    // Create canvas for plotting
+    auto c1 = new TCanvas("c1", "c1", 800, 700);
+    c1->SetLogy();
+
+    // Draw histogram
+    hist1->GetXaxis()->SetTitle("m_{#mu#mu} (GeV)");
+    hist1->GetXaxis()->SetTitleSize(0.04);
+    hist1->GetYaxis()->SetTitle("N_{Events}");
+    hist1->GetYaxis()->SetTitleSize(0.04);
+    hist1->SetStats(0);
+    hist1->SetFillColor(kOrange-3); 
+    hist1->SetLineColor(kOrange-3); 
+    hist1->SetMarkerStyle(20);
+    hist1->SetMarkerColor(kBlack); 
+    hist1->SetLineStyle(0);
+    hist1->DrawClone("PHF3");
+
+
+    // Draw labels
+    TLatex label1;
+    label1.SetTextSize(0.04);
+    label1.SetTextAlign(12);
+    label1.SetNDC(true);
+    label1.SetTextAlign(11);
+    label1.DrawLatex(0.45, 0.8, "Z");
+    label1.DrawLatex(0.10, 0.92, "CMS");
+    label1.DrawLatex(0.15, 0.8, "#bf{0.0<|y_{#mu#mu}|<0.4}");
+    label1.SetTextAlign(31);
+    label1.DrawLatex(0.90, 0.92, "#sqrt{s} = 8 TeV, L_{int} = 11.6 fb^{-1}");
+
+    //legend
+    auto legend1 = new TLegend(0.65,0.80,0.85,0.86);
+    legend1->AddEntry("hist1","MC:Z->#mu#mu","f");
+    legend1->SetBorderSize(0);
+    legend1->SetFillColor(0);
+    legend1->Draw();
+    
+    c1->SaveAs("images/dimuonspectrumZ/dimuonSpectrum_Z1.pdf");
+    report1->Print();
+
+    auto hist2 = df_mass2_MC.Histo1D({"hist2", "", bins, low, up}, "dimuon_mass");
+    // Request cut-flow report
+    auto report2 = df_mass2_MC.Report();
+
+    // Create canvas for plotting
+    auto c2 = new TCanvas("c2", "c2", 800, 700);
+    c2->SetLogy();
+
+    // Draw histogram
+    hist2->GetXaxis()->SetTitle("m_{#mu#mu} (GeV)");
+    hist2->GetXaxis()->SetTitleSize(0.04);
+    hist2->GetYaxis()->SetTitle("N_{Events}");
+    hist2->GetYaxis()->SetTitleSize(0.04);
+    hist2->SetStats(0);
+    hist2->SetFillColor(kOrange-3); 
+    hist2->SetLineColor(kOrange-3); 
+    hist2->SetMarkerStyle(20);
+    hist2->SetMarkerColor(kBlack); 
+    hist2->SetLineStyle(0);
+    hist2->DrawClone("PHF3");
+
+
+    // Draw labels
+    TLatex label2;
+    label2.SetTextSize(0.04);
+    label2.SetTextAlign(12);
+    label2.SetNDC(true);
+    label2.SetTextAlign(11);
+    label1.DrawLatex(0.45, 0.8, "Z");
+    label2.DrawLatex(0.10, 0.92, "CMS");
+    label2.DrawLatex(0.15, 0.8, "#bf{1.2<|y_{#mu#mu}|<1.8}");
+    label2.SetTextAlign(31);
+    label2.DrawLatex(0.90, 0.92, "#sqrt{s} = 8 TeV, L_{int} = 11.6 fb^{-1}");
+
+    //legend
+    auto legend2 = new TLegend(0.65,0.80,0.85,0.86);
+    legend2->AddEntry("hist2","MC:Z->#mu#mu","f");
+    legend2->SetBorderSize(0);
+    legend2->SetFillColor(0);
+    legend2->Draw();
+    
+    c2->SaveAs("images/dimuonspectrumZ/dimuonSpectrum_Z2.pdf");
+    report2->Print();
+
+    auto hist3 = df_mass3_MC.Histo1D({"hist3", "", bins, low, up}, "dimuon_mass");
+    // Request cut-flow report
+    auto report3 = df_mass3_MC.Report();
+
+    // Create canvas for plotting
+    auto c3 = new TCanvas("c3", "c3", 800, 700);
+    c3->SetLogy();
+
+    // Draw histogram
+    hist3->GetXaxis()->SetTitle("m_{#mu#mu} (GeV)");
+    hist3->GetXaxis()->SetTitleSize(0.04);
+    hist3->GetYaxis()->SetTitle("N_{Events}");
+    hist3->GetYaxis()->SetTitleSize(0.04);
+    hist3->SetStats(0);
+    hist3->SetFillColor(kOrange-3); 
+    hist3->SetLineColor(kOrange-3); 
+    hist3->SetMarkerStyle(20);
+    hist3->SetMarkerColor(kBlack); 
+    hist3->SetLineStyle(0);
+    hist3->DrawClone("PHF3");
+
+
+    // Draw labels
+    TLatex label3;
+    label3.SetTextSize(0.04);
+    label3.SetTextAlign(12);
+    label3.SetNDC(true);
+    label3.SetTextAlign(11);
+    label1.DrawLatex(0.45, 0.8, "Z");
+    label3.DrawLatex(0.10, 0.92, "CMS");
+    label3.DrawLatex(0.15, 0.8,"#bf{1.6<|y_{#mu#mu}|<2.0}");
+    label3.SetTextAlign(31);
+    label3.DrawLatex(0.90, 0.92, "#sqrt{s} = 8 TeV, L_{int} = 11.6 fb^{-1}");
+
+    //legend
+    auto legend3 = new TLegend(0.65,0.80,0.85,0.86);
+    legend3->AddEntry("hist3","MC:Z->#mu#mu","f");
+    legend3->SetBorderSize(0);
+    legend3->SetFillColor(0);
+    legend3->Draw();
+    
+    c3->SaveAs("images/dimuonspectrumZ/dimuonSpectrum_Z3.pdf");
+    report3->Print();
+}
