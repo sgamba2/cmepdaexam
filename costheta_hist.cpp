cea692ce960e898eda554e5519c8b652d2feb8d7
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-01-22 16:36:44 +0100

    Add files via upload

---
+++ costheta_hist.cpp
@@ -0,0 +1,116 @@
+#include "utilities.h"
+#include <ROOT/RDataFrame.hxx>
+#include <ROOT/RVec.hxx>
+#include <Math/Vector4Dfwd.h>
+#include <Math/Vector4D.h>
+#include <TCanvas.h>
+#include <TH1D.h>
+#include <TLatex.h>
+#include <TStyle.h>
+#include <TROOT.h>
+#include <TLegend.h>
+
+
+void costheta_hist(){
+    
+    /*
+    Parameters
+    ----------
+    It doesn't take any parameter
+
+    Returns
+    ---------
+    It is a void function and it generates three different 
+    plots (based on rapidity conditions) of the cos(theta*) of the dimuon system.
+    All the plots are saved in images/costheta/file.pdf.
+    */
+
+    //ROOT::EnableImplicitMT(1);
+    ROOT::RDataFrame df_MC("Events_new", "Events.root");
+
+    auto df_2mu_MC= allquantities(df_MC);
+    
+    auto df_2mu_MCy1= df_2mu_MC.Filter("fabs(y)<0.4 ", "y1");
+    auto df_2mu_MCy2= df_2mu_MC.Filter("fabs(y)<1.2 && fabs(y)>0.8", "y2");
+    auto df_2mu_MCy3= df_2mu_MC.Filter("fabs(y)<2.0 && fabs(y)>1.6", "y3");
+
+    auto c1 = new TCanvas("c1", "c1", 1000, 700);
+    constexpr int nbins = 40; //widthbins = 0.05
+    auto hist1 = df_2mu_MCy1.Histo1D({"hist1", "",nbins, -1, 1}, "costheta");
+    
+    hist1->SetFillColor(kOrange-3); 
+    hist1->SetLineColor(kOrange-3); 
+    hist1->SetMarkerStyle(20);
+    hist1->SetMarkerColor(kBlack); 
+    hist1->SetLineStyle(0);
+    hist1->GetXaxis()->SetTitle("cos(#theta*)");
+    hist1->GetXaxis()->SetTitleSize(0.04);
+    hist1->GetYaxis()->SetTitle("N_{Events} MC");
+    hist1->GetYaxis()->SetTitleSize(0.04);
+    hist1->SetStats(0);
+    hist1->DrawClone("PHF3");
+    TLatex label1;
+    label1.SetTextSize(0.04);
+    label1.SetNDC(true);
+    label1.DrawLatex(0.15, 0.85, "CMS");
+    label1.DrawLatex(0.15, 0.8, "#bf{0.0<|y_{#mu#mu}|<0.4}");
+    label1.DrawLatex(0.6, 0.94, "#sqrt{s} = 8 TeV, L_{int} = 18.8 fb^{-1}");
+    auto legend1 = new TLegend(0.65,0.80,0.85,0.86);
+    legend1->AddEntry("hist1","MC:Z->#mu#mu","f");
+    legend1->SetBorderSize(0);
+    legend1->SetFillColor(0);
+    legend1->Draw();
+    c1->SaveAs("images/costheta/costheta1.pdf");
+    
+    auto c2 = new TCanvas("c2", "c2", 1000, 700);
+    auto hist2 = df_2mu_MCy2.Histo1D({"hist2", "", nbins, -1, 1}, "costheta");
+    hist2->SetFillColor(kOrange-3); 
+    hist2->SetLineColor(kOrange-3); 
+    hist2->SetMarkerStyle(20);
+    hist2->SetMarkerColor(kBlack); 
+    hist2->SetLineStyle(0);
+    hist2->GetXaxis()->SetTitle("cos(#theta*)");
+    hist2->GetXaxis()->SetTitleSize(0.04);
+    hist2->GetYaxis()->SetTitle("N_{Events} MC");
+    hist2->GetYaxis()->SetTitleSize(0.04);
+    hist2->SetStats(0);
+    hist2->DrawClone("PHF3");
+    TLatex label2;
+    label2.SetTextSize(0.04);
+    label2.SetNDC(true);
+    label2.DrawLatex(0.15, 0.85, "CMS");
+    label2.DrawLatex(0.15, 0.8, "#bf{1.2<|y_{#mu#mu}|<1.8}");
+    label2.DrawLatex(0.6, 0.94, "#sqrt{s} = 8 TeV, L_{int} = 18.8 fb^{-1}");
+    auto legend2 = new TLegend(0.6,0.82,0.8,0.88);
+    legend2->SetBorderSize(0);
+    legend2->SetFillColor(0);
+    legend2->AddEntry("hist2","MC:Z->#mu#mu","f");
+    legend2->Draw();
+    c2->SaveAs("images/costheta/costheta2.pdf");
+
+    auto c3 = new TCanvas("c3", "c3", 1000, 700);
+    auto hist3 = df_2mu_MCy3.Histo1D({"hist3", "", nbins, -1, 1}, "costheta"); 
+    hist3->SetFillColor(kOrange-3); 
+    hist3->SetLineColor(kOrange-3); 
+    hist3->SetMarkerStyle(20);
+    hist3->SetMarkerColor(kBlack); 
+    hist3->SetLineStyle(0);
+    hist3->GetXaxis()->SetTitle("cos(#theta*)");
+    hist3->GetXaxis()->SetTitleSize(0.04);
+    hist3->GetYaxis()->SetTitle("N_{Events} MC");
+    hist3->GetYaxis()->SetTitleSize(0.04);
+    hist3->SetStats(0);
+    hist3->DrawClone("PHF3");
+    TLatex label3;
+    label3.SetTextSize(0.04);
+    label3.SetNDC(true);
+    label3.DrawLatex(0.15, 0.85, "CMS");
+    label3.DrawLatex(0.15, 0.8,"#bf{1.6<|y_{#mu#mu}|<2.0}");
+    label3.DrawLatex(0.6, 0.94, "#sqrt{s} = 8 TeV, L_{int} = 18.8 fb^{-1}");
+    auto legend3 = new TLegend(0.7,0.80,0.9,0.86);
+    legend3->AddEntry("hist3","MC:Z->#mu#mu","f");
+    legend3->SetBorderSize(0);
+    legend3->SetFillColor(0);
+    legend3->Draw();    
+    c3->SaveAs("images/costheta/costheta3.pdf");
+}
---
+++ utilities.h
@@ -0,0 +1,218 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+#include <ROOT/RDataFrame.hxx>
+#include "ROOT/RVec.hxx"
+#include "Math/Vector4Dfwd.h"
+#include "Math/Vector4D.h"
+
+using namespace ROOT::VecOps;
+
+float computeInvariantMass(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { 
+
+    /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates the four vector and it returns the dimuon invariant mass 
+    */
+    
+    ROOT::Math::PtEtaPhiMVector m1(pt[0], eta[0], phi[0], mass[0]);
+    ROOT::Math::PtEtaPhiMVector m2(pt[1], eta[1], phi[1], mass[1]);
+   
+    return (m1 + m2).mass();   
+}
+
+float computepztot(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { 
+
+    /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates a four vector and it returns the dimuon Pz 
+    */
+    
+    ROOT::Math::PtEtaPhiMVector m1(pt[0], eta[0], phi[0], mass[0]);
+    ROOT::Math::PtEtaPhiMVector m2(pt[1], eta[1], phi[1], mass[1]);
+
+    return (m1+m2).Pz();
+}
+
+float computepz1(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { 
+
+    /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates a fourvector and it returns the first muon Pz 
+    */
+    
+    ROOT::Math::PtEtaPhiMVector m1(pt[0], eta[0], phi[0], mass[0]);
+    
+    return m1.Pz();
+}
+
+float computepz2(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { 
+    
+    /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates a fourvector and it returns the second muon Pz 
+    */
+
+    ROOT::Math::PtEtaPhiMVector m2(pt[1], eta[1], phi[1], mass[1]);
+
+    return m2.Pz();
+}
+
+float computeEtot(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { //
+    
+     /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates a fourvector and it returns the energy of the dimuon system
+    */
+
+    ROOT::Math::PtEtaPhiMVector m1(pt[0], eta[0], phi[0], mass[0]);
+    ROOT::Math::PtEtaPhiMVector m2(pt[1], eta[1], phi[1], mass[1]);
+
+    return (m2+m1).E();
+}
+
+float computeE1(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { //
+    
+    /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates a fourvector and it returns the first muon energy
+    */
+
+    ROOT::Math::PtEtaPhiMVector m1(pt[0], eta[0], phi[0], mass[0]);
+    
+    return m1.E();
+}
+
+float computeE2(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { //
+    
+    /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates a fourvector and it returns the second muon energy 
+    */
+
+    ROOT::Math::PtEtaPhiMVector m2(pt[1], eta[1], phi[1], mass[1]);
+
+    return m2.E();
+}
+
+float computey(RVec<float>& pt, RVec<float>& eta, RVec<float>& phi, RVec<float>& mass) { //
+    
+    /*
+    Parameters
+    ----------
+    RVec<float>& pt : Muon_pt of the dataframe
+    RVec<float>& eta : Muon_eta of the dataframe
+    RVec<float>& phi : Muon_phi of the dataframe
+    RVec<float>& mass : Muon_mass of the dataframe
+
+    Returns
+    ---------
+    It creates a fourvector and it returns the dimuon rapidity
+    */
+
+    ROOT::Math::PtEtaPhiMVector m1(pt[0], eta[0], phi[0], mass[0]);
+    ROOT::Math::PtEtaPhiMVector m2(pt[1], eta[1], phi[1], mass[1]);
+
+    return (m1 + m2).Rapidity();
+}
+
+auto allquantities(ROOT::RDataFrame df){
+
+    /*
+    Parameters
+    ----------
+    ROOT::RDataFrame df: it takes a dataframe
+
+    Returns
+    ---------
+    It returns the first node of the computation graph for which the new quantity is defined.
+    This function adds new columns to the input dataframe, with new values as total energy or pz, 
+    first or second muon energy or pz, total rapidity, cos(theta*), pm1,pp1,pm2,pp2
+    */
+    
+    auto df_2mu_MC=df.Define("ptll","pow(pow(Muon_pt[0],2)+pow(Muon_pt[1],2),0.5)");
+
+    df_2mu_MC=df_2mu_MC.Define("dimuon_mass", computeInvariantMass,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+
+    df_2mu_MC=df_2mu_MC.Define("Pztot", computepztot,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+
+    df_2mu_MC=df_2mu_MC.Define("Pz1", computepz1,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+  
+    df_2mu_MC=df_2mu_MC.Define("Pz2", computepz2,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+
+    df_2mu_MC=df_2mu_MC.Define("E", computeEtot,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+
+    df_2mu_MC=df_2mu_MC.Define("E1", computeE1,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+
+    df_2mu_MC=df_2mu_MC.Define("E2", computeE2,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+  
+    df_2mu_MC=df_2mu_MC.Define("y", computey,{"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"});
+
+    df_2mu_MC=df_2mu_MC.Define("Pp1","(E1+Pz1)/pow(2,0.5)");
+
+    df_2mu_MC=df_2mu_MC.Define("Pp2","(E2+Pz2)/pow(2,0.5)");
+
+    df_2mu_MC=df_2mu_MC.Define("Pm1","(E1-Pz1)/pow(2,0.5)");
+
+    df_2mu_MC=df_2mu_MC.Define("Pm2","(E2-Pz2)/pow(2,0.5)");
+
+    df_2mu_MC=df_2mu_MC.Define("costheta", "2*(Pp1*Pm2-Pm1*Pp2)*Pztot/(pow(pow(dimuon_mass,2)*(pow(dimuon_mass,2)+pow(ptll,2)),0.5)*fabs(Pztot))");
+
+    return df_2mu_MC;
+}
+#endif
