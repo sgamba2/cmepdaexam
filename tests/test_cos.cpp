8885804c63e58802e7be0dfae7a94ed0964c6008
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-01-23 17:30:28 +0100

    Add files via upload

---
+++ tests/test_cos.cpp
@@ -0,0 +1,29 @@
+#include <ROOT/RDataFrame.hxx>
+#include <cmath>
+#include "../src/utilities.h"
+#include <ROOT/RVec.hxx>
+#include <Math/Vector4Dfwd.h>
+#include <Math/Vector4D.h>
+#include <vector>
+
+void test_cos(){
+    /*
+    Testing if cos is > or < 0
+    */
+    ROOT::EnableImplicitMT();
+
+    ROOT::RDataFrame df("Events_new", "../datas/Events.root");
+
+    auto df_2mu= allquantities(df);
+    auto df_2mucontrol=df_2mu.Filter("costheta>1 || costheta<-1","coseno control");
+    auto nEntries=df_2mucontrol.Count();
+
+    printf("testing if |costheta|<1 \n");
+
+    if( nEntries.GetValue()==0){
+        printf("test passed!\n");
+    }
+    else{
+        printf("test failed!\n");
+    }
+}
