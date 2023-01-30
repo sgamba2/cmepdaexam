a52b5c088437e4a0d18213338c31f309612d9e14
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-01-30 10:59:20 +0100

    Add files via upload

--- tests/test_filt.cpp
+++ tests/test_filt.cpp
@@ -12,6 +12,7 @@
 #include <Math/Vector4D.h>
 #include <vector>
 #include <iostream>
+#include <filesystem>
 
 void test_filt(){
 /******************************************************************************
@@ -21,7 +22,6 @@ void test_filt(){
 * \return None
 * 
 ******************************************************************************/
-
     //creating an empty dataframe with all columns and an empty dataframe with a missing column
     TFile file1("datas/SimpleTree1.root","RECREATE"); 
     TTree * tree1 = new TTree("Events","Events");
@@ -54,28 +54,28 @@ void test_filt(){
     file.Close();
 
     //test on number of columns
-    int test0 = filter_df("datas/SimpleTree1.root","datas/SimpleTree1.root","datas/MC.root","datas/dat.root");//few column 
-    int test1 = filter_df("datas/SimpleTree.root","datas/SimpleTree1.root","datas/MC.root","datas/dat.root");//looking for one file with few columns and one empty
-    int test2 = filter_df("datas/SimpleTree1.root","datas/SimpleTree.root","datas/MC.root","datas/dat.root");//looking for one file with few columns and one empty
+    int test0 = filter_df("datas/SimpleTree1.root","datas/SimpleTree1.root","MC.root","dat.root");//few column 
+    int test1 = filter_df("datas/SimpleTree.root","datas/SimpleTree1.root","MC.root","dat.root");//looking for one file with few columns and one empty
+    int test2 = filter_df("datas/SimpleTree1.root","datas/SimpleTree.root","MC.root","dat.root");//looking for one file with few columns and one empty
     
     //test on empty files
-    int test3 = filter_df("datas/SimpleTree.root","datas/SimpleTree.root","datas/MC.root","datas/dat.root");//looking for two empty files
-    int test4 = filter_df("datas/SimpleTree.root","datas/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/MC.root","datas/dat.root");//looking for one empty file
-    int test5 = filter_df("datas/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/SimpleTree.root","datas/MC.root","datas/dat.root");//looking for one empty file
+    int test3 = filter_df("datas/SimpleTree.root","datas/SimpleTree.root","MC.root","dat.root");//looking for two empty files
+    int test4 = filter_df("datas/SimpleTree.root","root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/MonteCarlo11_Summer11LegDR_DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola/0A55428E-3FFA-40D8-ABDE-BD877B402134.root","MC.root","dat.root");//looking for one empty file
+    int test5 = filter_df("root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/MonteCarlo11_Summer11LegDR_DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola/0A55428E-3FFA-40D8-ABDE-BD877B402134.root","datas/SimpleTree.root","MC.root","dat.root");//looking for one empty file
     
     //test on files with wrong path
-    int test6 = filter_df("datas/file.root","datas/file.root","datas/MC.root","datas/dat.root"); //looking for files that don't exist
-    int test7 = filter_df("dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/file.root","datas/MC.root","datas/dat.root"); //looking for a file exist and a file that doesn't exist
-    int test8 = filter_df("datas/file.root","dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/MC.root","datas/dat.root"); //looking for a file exist and a file that doesn't exist
-    int test9 = filter_df("dati/file.root","dati/file.root","datas/MC.root","datas/dat.root"); //looking for a file and a folder that don't exist
-    int test10 = filter_df("dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/MC.root","datas/dat.root");//looking for a folder that doesn't exist
-    int test11 = filter_df("datas/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/MC.root","datas/dat.root"); //looking for a file and a folder that exist
+    int test6 = filter_df("datas/file.root","datas/file.root","MC.root","dat.root"); //looking for files that don't exist
+    int test7 = filter_df("dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/file.root","MC.root","dat.root"); //looking for a file exist and a file that doesn't exist
+    int test8 = filter_df("datas/file.root","dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","MC.root","/dat.root"); //looking for a file exist and a file that doesn't exist
+    int test9 = filter_df("dati/file.root","dati/file.root","MC.root","dat.root"); //looking for a file and a folder that don't exist
+    int test10 = filter_df("dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","dati/00C074C0-1C19-4933-9407-5A05484E1F1E.root","MC.root","dat.root");//looking for a folder that doesn't exist
+    int test11 = filter_df("root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/MonteCarlo11_Summer11LegDR_DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola/0A55428E-3FFA-40D8-ABDE-BD877B402134.root","root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/MonteCarlo11_Summer11LegDR_DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola/0A55428E-3FFA-40D8-ABDE-BD877B402134.root","MC.root","dat.root"); //looking for a file and a folder that exist
 
     //test on files with a wrong extension
     TFile file2("datas/filedummy.txt","RECREATE");
-    int test12 = filter_df("datas/filedummy.txt","datas/filedummy.txt","datas/MC.root","datas/dat.root");//two files with wrong extention
-    int test13 = filter_df("datas/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/filedummy.txt","datas/MC.root","datas/dat.root");//second file with wrong extention
-    int test14 = filter_df("datas/filedummy.txt","datas/00C074C0-1C19-4933-9407-5A05484E1F1E.root","datas/MC.root","datas/dat.root");//first file with wrong extention
+    int test12 = filter_df("datas/filedummy.txt","datas/filedummy.txt","MC.root","dat.root");//two files with wrong extention
+    int test13 = filter_df("root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/MonteCarlo11_Summer11LegDR_DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola/0A55428E-3FFA-40D8-ABDE-BD877B402134.root","datas/filedummy.txt","MC.root","dat.root");//second file with wrong extention
+    int test14 = filter_df("datas/filedummy.txt","root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/MonteCarlo11_Summer11LegDR_DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola/0A55428E-3FFA-40D8-ABDE-BD877B402134.root","MC.root","dat.root");//first file with wrong extention
     
     if(test0==2 && test1==2 && test2==2 && test3==3 && test4==3 && test5==3 && test11==0 && test6==1 && test7==1 && test8==1 && test9==1 && test10==1 && test12==1 && test13==1 && test14==1){
         std::cout << "Test passed!\n";
