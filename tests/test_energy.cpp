d3ed94ea2e2ae9b91831e9550f3f8488f2e5019f
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-02-01 15:51:10 +0100

    Add files via upload

--- tests/test_energy.cpp
+++ tests/test_energy.cpp
@@ -24,29 +24,37 @@ void test_energy(std::string filepath_MC, std::string filepath_datas){
 * \return None
 * 
 ******************************************************************************/
-    //creating dataframes 
-    ROOT::RDataFrame df_MC("Events", filepath_MC);
-    ROOT::RDataFrame df_datas("Events", filepath_datas);
+    if(TFile::Open(filepath_datas.c_str())!=nullptr && TFile::Open(filepath_MC.c_str())!=nullptr){
+        //creating dataframes 
+        ROOT::RDataFrame df_MC("Events", filepath_MC);
+        ROOT::RDataFrame df_datas("Events", filepath_datas);
 
-    //defining all quantities
-    auto df_2mu_MC= allquantities(df_MC);
-    auto df_2mu_datas= allquantities(df_datas);
+        //checking if they have the right column
+        if( df_MC.HasColumn("Muon_pt") && df_MC.HasColumn("Muon_mass") && df_MC.HasColumn("Muon_phi") && df_MC.HasColumn("Muon_eta") &&  df_datas.HasColumn("Muon_pt") && df_datas.HasColumn("Muon_mass") && df_datas.HasColumn("Muon_phi")  && df_datas.HasColumn("Muon_eta") ){
+            //defining all quantities
+            auto df_2mu_MC= allquantities(df_MC);
+            auto df_2mu_datas= allquantities(df_datas);
 
-    //testing if sum of energies is almost equal to Etot
-    auto df_2mucontrol2_MC=df_2mu_MC.Filter("fabs(E1+E2-E)>0.00001*E","control"); 
-    auto df_2mucontrol2_datas=df_2mu_datas.Filter("fabs(E1+E2-E)>0.00001*E","control"); 
+            //testing if sum of energies is almost equal to Etot
+            auto df_2mucontrol2_MC=df_2mu_MC.Filter("fabs(E1+E2-E)>0.00001*E","control"); 
+            auto df_2mucontrol2_datas=df_2mu_datas.Filter("fabs(E1+E2-E)>0.00001*E","control"); 
 
-    //counting how many rows are left
-    auto nEntries_datas=df_2mucontrol2_datas.Count();
-    auto nEntries_MC=df_2mucontrol2_MC.Count();
+            //counting how many rows are left
+            auto nEntries_datas=df_2mucontrol2_datas.Count();
+            auto nEntries_MC=df_2mucontrol2_MC.Count();
 
-    std::cout<<"Testing if sum of energies is almost equal to Etot \n";
+            std::cout<<"Testing if sum of energies is almost equal to Etot \n";
     
-    if( nEntries_MC.GetValue()==0 && nEntries_datas.GetValue()==0){
-        std::cout<<"Test passed! \n";
-    }
-    else{
-        std::cout<<"Test failed! \n";
+            if( nEntries_MC.GetValue()==0 && nEntries_datas.GetValue()==0){
+                std::cout<<"Test passed! \n";
+            }else{
+                std::cout<<"Test failed! \n";
+            }
+        }else{
+            std::cout << "Your file haven't the right columns\n";
+        }
+    }else{
+        std::cout << "Your files don't exist, change datas!\n";
     }
 }
 
