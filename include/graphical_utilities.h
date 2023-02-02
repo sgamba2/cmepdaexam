85cc0991c07d143d1af2490c520e015ed681a553
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-02-02 22:39:27 +0100

    Add files via upload

--- include/graphical_utilities.h
+++ include/graphical_utilities.h
@@ -312,13 +312,14 @@ auto operationhist( ROOT::RDF::RResultPtr<::TH2D> & histNf, ROOT::RDF::RResultPt
 }
 
 
-void afbhist(ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> df_MC, string filename, string rapiditylim){
+void afbhist(ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> df_MC, ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> df_datas, int filenumber, string rapiditylim){
 /******************************************************************************
 * 
 * \brief creating Afb histogram 
 * 
 * @param df_MC: dataframe of the MonteCarlo datas
-* @param filename: name of the final file
+* @param df_datas: dataframe of the RUN datas
+* @param filenumber: number of the rapidity condition 
 * @param rapiditylim: limit of rapidity to write on the canvas
 * 
 * \return None
@@ -327,26 +328,71 @@ void afbhist(ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> df_MC
    //creating two filtered datframes, one with costheta=>0, one with costheta<0
    auto df_cm_MC=df_MC.Filter("costheta<0", "backward");
    auto df_cp_MC=df_MC.Filter("costheta>=0", "forward");
-   
+   auto df_cm_datas=df_datas.Filter("costheta<0", "backward");
+   auto df_cp_datas=df_datas.Filter("costheta>=0", "forward");
+
+   //creating report
+   auto report_cm_MC = df_cm_MC.Report();
+   report_cm_MC->Print();
+   auto report_cm_datas = df_cm_datas.Report();
+   report_cm_datas->Print();
+   auto report_cp_MC = df_cp_MC.Report();
+   report_cp_MC->Print();
+   auto report_cp_datas = df_cp_datas.Report();
+   report_cp_datas->Print();
+
    //creating four histogram 2D of the dimuonmass and rapidity weighted with wn
    auto histDf_MC = df_cp_MC.Histo2D({"cp,Df", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wd");
    auto histNf_MC = df_cp_MC.Histo2D({"cp,Nf", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wn");
    auto histDb_MC = df_cm_MC.Histo2D({"cm,Db", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wd");
    auto histNb_MC = df_cm_MC.Histo2D({"cm,Nb", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wn");
+   auto histDf_datas = df_cp_datas.Histo2D({"cp,Df", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wd");
+   auto histNf_datas = df_cp_datas.Histo2D({"cp,Nf", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wn");
+   auto histDb_datas = df_cm_datas.Histo2D({"cm,Db", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wd");
+   auto histNb_datas = df_cm_datas.Histo2D({"cm,Nb", "", 5, 60,120,5,-2.4,2.4},"dimuon_mass","y","wn");
+
+   //normalizing histograms
+   histDf_MC->Scale(1/float((df_cp_MC.Count()).GetValue()));
+   histNf_MC->Scale(1/float((df_cp_MC.Count()).GetValue()));
+   histDb_MC->Scale(1/float((df_cm_MC.Count()).GetValue()));
+   histNb_MC->Scale(1/float((df_cm_MC.Count()).GetValue()));
+
+   histDf_datas->Scale(1/float((df_cp_datas.Count()).GetValue()));
+   histNf_datas->Scale(1/float((df_cp_datas.Count()).GetValue()));
+   histDb_datas->Scale(1/float((df_cm_datas.Count()).GetValue()));
+   histNb_datas->Scale(1/float((df_cm_datas.Count()).GetValue()));
 
    //operation on histogram
    auto hist_MC=operationhist(histNf_MC, histDf_MC, histNb_MC, histDb_MC);
+   auto hist_datas=operationhist(histNf_datas, histDf_datas, histNb_datas, histDb_datas);
+   
+
+   //drawing two subpads
+   auto pad1 = new TPad("pad1","pad1",0.06,0.33,1,1);
+   auto pad2 = new TPad("pad2","pad2",0.06,0.08,1,0.30);
+   pad1->SetBorderMode(0);
+   pad2->SetTopMargin(0.00001);
+   pad2->SetBorderMode(0);
+   pad2->SetLeftMargin(0.17);
+   pad1->SetLeftMargin(0.17);
+   pad2->SetBottomMargin(0.2);
+   pad1->Draw();
+   pad2->Draw();
+
+   //fill pad1 with the projections
+   pad1->cd();
    
    //projection of the final histogram
-   auto h_MC= hist_MC->ProjectionX();
+   auto h_MC= hist_MC->ProjectionX("MC");
+   auto h_datas= hist_datas->ProjectionX("datas");
 
    //graphical set
-   h_MC->SetStats(0);;
-   h_MC->SetMarkerStyle(8);
-   h_MC->SetMarkerColor(1);
+   h_MC->SetStats(0); 
+   h_datas->SetStats(0);
 
-   h_MC->GetXaxis()->SetTitle("m_{#mu#mu}");
-   h_MC->GetYaxis()->SetTitle("Afb MC");
+   h_MC->SetLineColor(2);
+   h_MC->GetXaxis()->SetTitle("m_{#mu#mu} (GeV)");
+   h_MC->GetYaxis()->SetTitle("Afb");
 
    h_MC->GetXaxis()->CenterTitle(true);
    h_MC->GetYaxis()->CenterTitle(true);
@@ -359,20 +405,63 @@ void afbhist(ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> df_MC
    h_MC->GetXaxis()->SetTitleOffset(0.34);
    h_MC->GetXaxis()->SetTickLength(0.05);
 
-   
+   h_MC->GetYaxis()->SetRangeUser(-1.2, 1.2);
    h_MC->GetYaxis()->SetLabelSize(0.09);
-   h_MC->GetYaxis()->SetTitleSize(0.11);
-   h_MC->GetYaxis()->SetTitleOffset(0.66);
+   h_MC->GetYaxis()->SetTitleSize(0.13);
+   h_MC->GetYaxis()->SetLabelOffset(0.01);
+   h_MC->GetYaxis()->SetTitleOffset(0.6);
    h_MC->GetYaxis()->SetTickLength(0.05);
+   
+   h_datas->SetMarkerColor(1); 
+   h_datas->SetLineColor(1); 
+   h_datas->SetMarkerStyle(8); 
 
    //drawing the final histogram
-   h_MC->DrawClone("PSAME");
+   h_MC->DrawClone("HIST SAME");
+   h_datas->DrawClone("P SAME");
+
+   //legend only on the first pad
+   if (filenumber == 1){
+
+      auto legend = new TLegend(0.3,0.3,0.5,0.45);
+      legend->AddEntry(h_MC,"MC","l");
+      legend->AddEntry(h_datas,"Datas","lep");
+      legend->SetBorderSize(0);
+      legend->SetFillColor(0);
+      legend->SetTextSize(0.13);
+      legend->Draw();
+
+   }
 
    //label on the histogram
    TLatex label;
-   label.SetTextSize(0.1);
+   label.SetTextSize(0.11);
    label.SetTextAlign(12);
    label.SetNDC(true);
-   label.DrawLatex(0.3, 0.85, rapiditylim.c_str());
+   label.DrawLatex(0.3, 0.2, rapiditylim.c_str());
+
+   //residual pad
+   pad2->cd();
+   pad2->SetGridy();
+   
+   //creating histo Data-MC
+   h_datas->Add(h_MC,-1.);
+
+   //setting histogram properties
+   h_datas->SetStats(0);
+   
+   h_datas->GetXaxis()->SetTitle("m_{#mu#mu} (GeV)");
+   h_datas->GetXaxis()->SetLabelSize(0.10);
+   h_datas->GetXaxis()->SetNdivisions(505);
+   h_datas->GetXaxis()->SetTitleSize(0.11);
+   h_datas->GetXaxis()->SetTitleOffset(0.86);
+   
+   h_datas->GetYaxis()->SetTitle("Data-MC");
+   h_datas->GetYaxis()->SetLabelSize(0.08);
+   h_datas->GetYaxis()->SetNdivisions(505);
+   h_datas->GetYaxis()->SetTitleOffset(0.6);
+   h_datas->GetYaxis()->SetTitleSize(0.12);
+
+   h_datas->DrawClone("PESAME");
 }
 #endif /* GRAPHICAL_UTILITIES_H */
