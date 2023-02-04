880a630f8677daf5b112f1cd0fdabcfc964e7b57
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-02-04 17:46:24 +0100

    Add files via upload

--- openfiles.py
+++ openfiles.py
@@ -3,6 +3,7 @@
 #
 # We enabled multi-threading. The default here is set to a single thread. You can choose the number of threads based on your system.
 #
+
 import argparse
 import ROOT
 
@@ -13,72 +14,76 @@ def process(choice, filepath_MC, filepath_datas, filename_MC_fil, filename_datas
     This function takes six strings given by argparse, which the user will give from terminal. It will process datas
     and it will do the test chosen by the user.
    
-    choice: could be 'y' or 'n', if 'y' the program will filter the datas, if 'n' it will look for 
+    choice: string that could be 'y' or 'n', if 'y' the program will filter the datas, if 'n' it will look for 
     files already filtered
-    filepath_MC: the path of the MC file to filter
-    filepath_datas: the path of the RUN file to filter
-    filename_MC_fil: the name of the MC file already filtered
-    filename_datas_fil: the name of the RUN file already filtered
-    analysis: it is a string that represents the test I want to do  0: no analysis, 
+    filepath_MC: string of the path of the MC file to filter
+    filepath_datas: string of the path of the RUN file to filter
+    filename_MC_fil: string of the name of the MC file already filtered
+    filename_datas_fil: string of the name of the RUN file already filtered
+    analysis: string that represents the test I want to do  0: no analysis, 
     cos: costheta histograms, dimspec: Dimuon spectrum of Z, afb: Asimmetry forward backward
     
     return: None
     '''
+
     #only if fil is equal to 0 I can process the datas
-    fil=0
+    fil = 0
     if(choice == 'y'):
         ROOT.gInterpreter.ProcessLine('.L src/filter_df.cpp')
         fil=ROOT.filter_df(filepath_MC, filepath_datas, filename_MC_fil, filename_datas_fil)
         
-    filename_MC_fil='datas/'+filename_MC_fil
-    filename_datas_fil='datas/'+filename_datas_fil
+    filename_MC_fil = 'datas/' + filename_MC_fil
+    filename_datas_fil = 'datas/' + filename_datas_fil
     
-    if fil==0:
+    if fil == 0:
         if(analysis == '0'):
             print('Goodbye!')  
             quit()
+
         if(analysis == 'cos'): 
             ROOT.gInterpreter.ProcessLine('.L src/costheta_hist.cpp')
-            ROOT.costheta_hist(filename_MC_fil,filename_datas_fil)
+            ROOT.costheta_hist(filename_MC_fil, filename_datas_fil)
             print('If the process was fine, you will find your files in images/costheta. Goodbye!')  
+
         if(analysis == 'dimspec'): 
             ROOT.gInterpreter.ProcessLine('.L src/dimuon_spectrum_Z.cpp') 
-            ROOT.dimuon_spectrum_Z(filename_MC_fil,filename_datas_fil)
-            print('If the process was fine, you will find your files in images/dimuonspectrum. Goodbye!')  
+            ROOT.dimuon_spectrum_Z(filename_MC_fil, filename_datas_fil)
+            print('If the process was fine, you will find your files in images/dimuonspectrum. Goodbye!') 
+
         if(analysis == 'afb'): 
             ROOT.gInterpreter.ProcessLine('.L src/afb.cpp')
-            ROOT.afb(filename_MC_fil,filename_datas_fil)
+            ROOT.afb(filename_MC_fil, filename_datas_fil)
             print('If the process was fine, you will find your files in images/afb. Goodbye!')  
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Study of the CMS Monte Carlo and Run open data for the dimuon system')
+    parser = argparse.ArgumentParser(description = 'Study of the CMS Monte Carlo and Run open data for the dimuon system')
 
-    parser.add_argument('--filter', type=str, help='Do you want to filter your datas? [y/n]')
-    parser.add_argument('--filterMC', type=str, help='If filter is y: Choose the path of your MC dataframe (with .root extension) \nif filter is n: write none ')
-    parser.add_argument('--filterRUN', type=str, help='Choose the path of your RUN dataframe (with .root extension)')
-    parser.add_argument('--MCfile', type=str, help='Choose the name of your filtered MC dataframe in datas (with .root extension)')
-    parser.add_argument('--RUNfile', type=str, help='Choose the name of your filtered RUN dataframe in datas (with .root extension)')
-    parser.add_argument('--analysis', type=str, help='Which analysis do you want to do? 0: no analysis, cos: costheta histograms, dimspec: Dimuon spectrum of Z, afb: Asimmetry forward backward')
+    parser.add_argument('--filter', type = str, help = 'Do you want to filter your datas? [y/n]')
+    parser.add_argument('--filterMC', type = str, help = 'If filter is y: Choose the path of your MC dataframe (with .root extension) \nif filter is n: write none ')
+    parser.add_argument('--filterRUN', type = str, help = 'Choose the path of your RUN dataframe (with .root extension)')
+    parser.add_argument('--MCfile', type = str, help = 'Choose the name of your filtered MC dataframe in datas (with .root extension)')
+    parser.add_argument('--RUNfile', type = str, help = 'Choose the name of your filtered RUN dataframe in datas (with .root extension)')
+    parser.add_argument('--analysis', type = str, help = 'Which analysis do you want to do? 0: no analysis, cos: costheta histograms, dimspec: Dimuon spectrum of Z, afb: Asimmetry forward backward')
     
     args = parser.parse_args()
 
     #if this stats are true I can process datas and make analysis
-    if args.filter is None or args.analysis is None:
+    if(args.filter is None or args.analysis is None):
         print('Cannot start the program: missing one of the arguments (filter or analysis)!')
-    elif(args.filter!= 'n' and args.filter!= 'y'):
+    elif(args.filter != 'n' and args.filter != 'y'):
         print('Cannot start the program: filter is incorrect, please choose y or n!')
-    elif(args.analysis!= '0' and args.analysis!= 'cos' and args.analysis!= 'dimspec' and args.analysis!= 'afb'):
+    elif(args.analysis != '0' and args.analysis != 'cos' and args.analysis != 'dimspec' and args.analysis != 'afb'):
         print('Cannot start the program: analysis is incorrect! Please choose between 0, dimspec, cos or afb')
     elif(args.filter == 'y' and (args.filterMC is None or args.filterRUN is None or args.MCfile is None or args.RUNfile is None)):
         print('Cannot start the program: missing the path of your files or the name of your filtered files!')
-    elif(args.filter=='n' and (args.MCfile is None or args.RUNfile is None)):
+    elif(args.filter == 'n' and (args.MCfile is None or args.RUNfile is None)):
         print('Cannot start the program: few arguments!')
-    elif(args.filter=='n'):
-       process(args.filter,'','',args.MCfile, args.RUNfile,args.analysis) 
+    elif(args.filter == 'n'):
+       process(args.filter, ' ', ' ', args.MCfile, args.RUNfile, args.analysis) 
     else:
-        process(args.filter,args.filterMC,args.filterRUN,args.MCfile, args.RUNfile,args.analysis)
+        process(args.filter, args.filterMC, args.filterRUN, args.MCfile, args.RUNfile, args.analysis)
   
         
 
--- testing.py
+++ testing.py
@@ -3,6 +3,7 @@
 #
 # We enabled multi-threading. The default here is set to a single thread. You can choose the number of threads based on your system.
 #
+
 import argparse
 import ROOT
 
@@ -13,13 +14,13 @@ def process(choice, filepath_MC, filepath_datas, filename_MC_fil, filename_datas
     This function takes six strings given by argparse, which the user will give from terminal. It will process datas
     and it will do the test chosen by the user.
    
-    choice: could be 'y' or 'n', if 'y' the program will filter the datas, if 'n' it will look for 
+    choice: string that could be 'y' or 'n', if 'y' the program will filter the datas, if 'n' it will look for 
     files already filtered (if testopt is not equal to 1)
-    filepath_MC: the path of the MC file to filter
-    filepath_datas: the path of the RUN file to filter
-    filename_MC_fil: the name of the MC file already filtered
-    filename_datas_fil: the name of the RUN file already filtered
-    testopt: it is a string that represents the test I want to do 0: no test, 1: test on filter function 
+    filepath_MC: string of the path of the MC file to filter
+    filepath_datas: string of the path of the RUN file to filter
+    filename_MC_fil: string of the name of the MC file already filtered
+    filename_datas_fil: string of the name of the RUN file already filtered
+    testopt: string that represents the test I want to do 0: no test, 1: test on filter function 
     (no files input needed), 2: costheta test (files needed), 3: test on energy (files needed), 
     4: test on energy formulas (files needed)
     
@@ -32,35 +33,35 @@ def process(choice, filepath_MC, filepath_datas, filename_MC_fil, filename_datas
         ROOT.gInterpreter.ProcessLine('.L src/filter_df.cpp')
         fil=ROOT.filter_df(filepath_MC, filepath_datas, filename_MC_fil, filename_datas_fil)
    
-    filename_MC_fil='datas/'+filename_MC_fil
-    filename_datas_fil='datas/'+filename_datas_fil
+    filename_MC_fil = 'datas/' + filename_MC_fil
+    filename_datas_fil = 'datas/' + filename_datas_fil
 
     #if the filtered datas returns 0 you can access your filtered datas, if you chose not to filter datas, fil is already 0
-    if fil==0:
-        if (testopt=='0'):
+    if fil == 0:
+        if (testopt == '0'):
             print('Goodbye!')
             quit()
-        if (testopt=='1'): 
+        if (testopt == '1'): 
             ROOT.gInterpreter.ProcessLine('.L tests/test_filt.cpp')
             ROOT.test_filt()
             print('Goodbye!')
             quit()
-        if (testopt=='2'): 
+        if (testopt == '2'): 
             ROOT.gInterpreter.ProcessLine('.L tests/test_cos.cpp')
             ROOT.test_cos(filename_MC_fil, filename_datas_fil)
             print('Goodbye!')
             quit()
-        if (testopt=='3'): 
+        if (testopt == '3'): 
             ROOT.gInterpreter.ProcessLine('.L tests/test_energy.cpp') 
             ROOT.test_energy(filename_MC_fil, filename_datas_fil)
             print('Goodbye!')
             quit()
-        if (testopt=='4'):
+        if (testopt == '4'):
             ROOT.gInterpreter.ProcessLine('.L tests/test_energy_formulas.cpp')
             ROOT.test_energy_formulas(filename_MC_fil, filename_datas_fil)
             print('Goodbye!')
             quit()
-        if (testopt=='5'):
+        if (testopt == '5'):
             ROOT.gInterpreter.ProcessLine('.L tests/test_operationhist.cpp')
             ROOT.test_operationhist()
             print('Goodbye!')
@@ -69,14 +70,14 @@ def process(choice, filepath_MC, filepath_datas, filename_MC_fil, filename_datas
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Tests on the study of the CMS Monte Carlo and Run open data for the dimuon system')
+    parser = argparse.ArgumentParser(description = 'Tests on the study of the CMS Monte Carlo and Run open data for the dimuon system')
 
-    parser.add_argument('--filter', type=str, help='Do you want to filter your datas? [y/n]')
-    parser.add_argument('--filterMC', type=str, help='If filter is y: Choose the path of your MC dataframe (with .root extension) ')
-    parser.add_argument('--filterRUN', type=str, help='If filter is y: Choose the path of your RUN dataframe (with .root extension)')
-    parser.add_argument('--MCfile', type=str, help='Choose the name of your filtered MC dataframe in datas (with .root extension)')
-    parser.add_argument('--RUNfile', type=str, help='Choose the name of your filtered RUN dataframe in datas (with .root extension)')
-    parser.add_argument('--testopt', type=str, help='Which test do you want to do? 0: no test, 1: test on filter function (no files needed), 2: costheta test (files needed), 3: test on energy (files needed), 4: test on energy formulas (files needed), 5: test on operation_hist (no files needed)')
+    parser.add_argument('--filter', type = str, help = 'Do you want to filter your datas? [y/n]')
+    parser.add_argument('--filterMC', type = str, help = 'If filter is y: Choose the path of your MC dataframe (with .root extension) ')
+    parser.add_argument('--filterRUN', type = str, help = 'If filter is y: Choose the path of your RUN dataframe (with .root extension)')
+    parser.add_argument('--MCfile', type = str, help = 'Choose the name of your filtered MC dataframe in datas (with .root extension)')
+    parser.add_argument('--RUNfile', type = str, help = 'Choose the name of your filtered RUN dataframe in datas (with .root extension)')
+    parser.add_argument('--testopt', type = str, help = 'Which test do you want to do? 0: no test, 1: test on filter function (no files needed), 2: costheta test (files needed), 3: test on energy (files needed), 4: test on energy formulas (files needed), 5: test on operation_hist (no files needed)')
     
     args = parser.parse_args()
 
@@ -85,19 +86,19 @@ if __name__ == '__main__':
         print('Cannot start the program: missing the testopt!')
     elif(args.filter is None and args.testopt != '1' and args.testopt != '5'):
         print('Cannot start the program: missing one of the arguments (filter or testopt)!')
-    elif(args.filter!= 'n' and args.filter!= 'y' and args.testopt != '1' and args.testopt != '5'):
+    elif(args.filter != 'n' and args.filter != 'y' and args.testopt != '1' and args.testopt != '5'):
         print('Cannot start the program: filter is incorrect!')
-    elif(args.testopt!= '0' and args.testopt!= '1' and args.testopt!= '2' and args.testopt!= '3' and args.testopt!= '4' and args.testopt != '5'):
+    elif(args.testopt != '0' and args.testopt != '1' and args.testopt != '2' and args.testopt != '3' and args.testopt != '4' and args.testopt != '5'):
         print('Cannot start the program: testopt is incorrect!')
     elif(args.filter == 'y' and (args.filterMC is None or args.filterRUN is None or args.MCfile is None or args.RUNfile is None) and args.testopt != '1' and args.testopt != '5'):
         print('Cannot start the program: missing the path of your files or the name of your filtered files!')
-    elif(args.filter=='n' and (args.MCfile is None or args.RUNfile is None) and args.testopt != '1' and args.testopt != '5'):
+    elif(args.filter == 'n' and (args.MCfile is None or args.RUNfile is None) and args.testopt != '1' and args.testopt != '5'):
         print('Cannot start the program: few arguments!')
-    elif(args.testopt=='1'):
-        process('n','','','','',args.testopt)
-    elif(args.testopt=='5'):
-        process('n','','','','',args.testopt)
-    elif(args.filter=='n' and args.testopt != '1' and args.testopt != '5'):
-        process(args.filter,'','',args.MCfile,args.RUNfile,args.testopt)
+    elif(args.testopt == '1'):
+        process('n', ' ', ' ', ' ', ' ', args.testopt)
+    elif(args.testopt == '5'):
+        process('n', ' ', ' ', ' ', ' ', args.testopt)
+    elif(args.filter == 'n' and args.testopt != '1' and args.testopt != '5'):
+        process(args.filter, ' ', ' ', args.MCfile, args.RUNfile, args.testopt)
     else:
-        process(args.filter,args.filterMC,args.filterRUN,args.MCfile, args.RUNfile,args.testopt)
+        process(args.filter, args.filterMC, args.filterRUN, args.MCfile, args.RUNfile, args.testopt)
