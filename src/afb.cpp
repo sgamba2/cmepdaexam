c0e07110a6a99863f6dd8f70ff9e25fd15c77aea
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-02-04 21:01:11 +0100

    Add files via upload

--- src/afb.cpp
+++ src/afb.cpp
@@ -20,56 +20,56 @@
 void afb(std::string filepath_MC, std::string filepath_datas){
 
     //checking the correct path
-    if( TFile::Open(filepath_MC.c_str())!=nullptr && TFile::Open(filepath_datas.c_str())!=nullptr){
+    if( TFile::Open(filepath_MC.c_str()) != nullptr && TFile::Open(filepath_datas.c_str()) != nullptr){
 
         //creating the dataframe
         ROOT::RDataFrame df_MC("Events", filepath_MC);
         ROOT::RDataFrame df_datas("Events", filepath_datas);
         
         //checking if there are the right columns
-        if( df_MC.HasColumn("Muon_pt") && df_MC.HasColumn("Muon_mass") && df_MC.HasColumn("Muon_phi") && df_MC.HasColumn("Muon_eta") && df_datas.HasColumn("Muon_pt") && df_datas.HasColumn("Muon_mass") && df_datas.HasColumn("Muon_phi") && df_datas.HasColumn("Muon_eta") ){
+        if(df_MC.HasColumn("Muon_pt") && df_MC.HasColumn("Muon_mass") && df_MC.HasColumn("Muon_phi") && df_MC.HasColumn("Muon_eta") && df_datas.HasColumn("Muon_pt") && df_datas.HasColumn("Muon_mass") && df_datas.HasColumn("Muon_phi") && df_datas.HasColumn("Muon_eta")){
             
             //defining all quantities
             auto df_2mu_MC = allquantities(df_MC);
-            df_2mu_MC=df_2mu_MC.Define("wd","0.5*pow(costheta,2)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),3)");
-            df_2mu_MC=df_2mu_MC.Define("wn","0.5*fabs(costheta)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),2)");
+            df_2mu_MC = df_2mu_MC.Define("wd","0.5*pow(costheta,2)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),3)");
+            df_2mu_MC = df_2mu_MC.Define("wn","0.5*fabs(costheta)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),2)");
 
             auto df_2mu_datas = allquantities(df_datas);
-            df_2mu_datas=df_2mu_datas.Define("wd","0.5*pow(costheta,2)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),3)");
-            df_2mu_datas=df_2mu_datas.Define("wn","0.5*fabs(costheta)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),2)");
+            df_2mu_datas = df_2mu_datas.Define("wd", "0.5*pow(costheta,2)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),3)");
+            df_2mu_datas = df_2mu_datas.Define("wn", "0.5*fabs(costheta)/pow((pow(costheta,2)+1+0.005*(1-3*pow(costheta,2))),2)");
             
             //filter the dataframe
-            auto df_2mu_MC1= df_2mu_MC.Filter("fabs(y)<=0.4 ", "y1");
-            auto df_2mu_MC2= df_2mu_MC.Filter("fabs(y)<=0.8 && fabs(y)>0.4", "y2");
-            auto df_2mu_MC3= df_2mu_MC.Filter("fabs(y)<=1.2 && fabs(y)>0.8", "y3");
-            auto df_2mu_MC4= df_2mu_MC.Filter("fabs(y)<=1.6 && fabs(y)>1.2", "y4");
-            auto df_2mu_MC5= df_2mu_MC.Filter("fabs(y)<=2.0 && fabs(y)>1.6", "y5");
-            auto df_2mu_MC6= df_2mu_MC.Filter("fabs(y)<=2.4 && fabs(y)>2.0", "y6");
-            auto df_2mu_datas1= df_2mu_datas.Filter("fabs(y)<=0.4 ", "y1");
-            auto df_2mu_datas2= df_2mu_datas.Filter("fabs(y)<=0.8 && fabs(y)>0.4", "y2");
-            auto df_2mu_datas3= df_2mu_datas.Filter("fabs(y)<=1.2 && fabs(y)>0.8", "y3");
-            auto df_2mu_datas4= df_2mu_datas.Filter("fabs(y)<=1.6 && fabs(y)>1.2", "y4");
-            auto df_2mu_datas5= df_2mu_datas.Filter("fabs(y)<=2.0 && fabs(y)>1.6", "y5");
-            auto df_2mu_datas6= df_2mu_datas.Filter("fabs(y)<=2.4 && fabs(y)>2.0", "y6");
+            auto df_2mu_MC1 = df_2mu_MC.Filter("fabs(y)<=0.4 ", "y1");
+            auto df_2mu_MC2 = df_2mu_MC.Filter("fabs(y)<=0.8 && fabs(y)>0.4", "y2");
+            auto df_2mu_MC3 = df_2mu_MC.Filter("fabs(y)<=1.2 && fabs(y)>0.8", "y3");
+            auto df_2mu_MC4 = df_2mu_MC.Filter("fabs(y)<=1.6 && fabs(y)>1.2", "y4");
+            auto df_2mu_MC5 = df_2mu_MC.Filter("fabs(y)<=2.0 && fabs(y)>1.6", "y5");
+            auto df_2mu_MC6 = df_2mu_MC.Filter("fabs(y)<=2.4 && fabs(y)>2.0", "y6");
+            auto df_2mu_datas1 = df_2mu_datas.Filter("fabs(y)<=0.4 ", "y1");
+            auto df_2mu_datas2 = df_2mu_datas.Filter("fabs(y)<=0.8 && fabs(y)>0.4", "y2");
+            auto df_2mu_datas3 = df_2mu_datas.Filter("fabs(y)<=1.2 && fabs(y)>0.8", "y3");
+            auto df_2mu_datas4 = df_2mu_datas.Filter("fabs(y)<=1.6 && fabs(y)>1.2", "y4");
+            auto df_2mu_datas5 = df_2mu_datas.Filter("fabs(y)<=2.0 && fabs(y)>1.6", "y5");
+            auto df_2mu_datas6 = df_2mu_datas.Filter("fabs(y)<=2.4 && fabs(y)>2.0", "y6");
     
             //setting canvas
-            auto c = new TCanvas("c","",1000,800);
+            auto c = new TCanvas("c", "", 1000, 800);
 
             //labels
             TLatex label;
             label.SetTextSize(0.05);
             label.SetTextAlign(12);
             label.SetNDC(true);
-            label.DrawLatex(0.10, 0.93, "CMS");
-            label.DrawLatex(0.40, 0.93, "#sqrt{s} = 8 TeV, L_{int} = 18.8 fb^{-1}");
+            label.DrawLatex(0.10, 0.94, "CMS");
+            label.DrawLatex(0.40, 0.94, "#sqrt{s} = 8 TeV, L_{int} = 18.8 fb^{-1}");
 
             //setting pads
-            auto pad1 = new TPad("pad1","pad1",0.03,0,0.18,0.9);
-            auto pad2 = new TPad("pad2","pad2",0.19,0,0.34,0.9);
-            auto pad3 = new TPad("pad3","pad3",0.35,0,0.50,0.9);
-            auto pad4 = new TPad("pad4","pad4",0.52,0,0.67,0.9);
-            auto pad5 = new TPad("pad5","pad5",0.69,0,0.84,0.9);
-            auto pad6 = new TPad("pad6","pad6",0.86,0,1,0.9);
+            auto pad1 = new TPad("pad1", "pad1", 0.03, 0., 0.18, 0.9);
+            auto pad2 = new TPad("pad2", "pad2", 0.19, 0., 0.34, 0.9);
+            auto pad3 = new TPad("pad3", "pad3", 0.35, 0., 0.50, 0.9);
+            auto pad4 = new TPad("pad4", "pad4", 0.52, 0., 0.67, 0.9);
+            auto pad5 = new TPad("pad5", "pad5", 0.69, 0., 0.84, 0.9);
+            auto pad6 = new TPad("pad6", "pad6", 0.86, 0., 1.00, 0.9);
             pad1->SetTopMargin(0.1);
             pad1->SetLeftMargin(0.20);
             pad1->SetBottomMargin(0.1);
@@ -103,25 +103,25 @@ void afb(std::string filepath_MC, std::string filepath_datas){
 
             //drawing histograms
             pad1->cd();
-            afbhist(df_2mu_MC1,df_2mu_datas1,1,"#bf{0.0<|y_{#mu#mu}|<0.4}");
+            afbhist(df_2mu_MC1, df_2mu_datas1, 1, "#bf{0.0<|y_{#mu#mu}|<0.4}");
             pad2->cd();
-            afbhist(df_2mu_MC2,df_2mu_datas2,2,"#bf{0.4<|y_{#mu#mu}|<0.8}");
+            afbhist(df_2mu_MC2, df_2mu_datas2, 2, "#bf{0.4<|y_{#mu#mu}|<0.8}");
             pad3->cd();
-            afbhist(df_2mu_MC3,df_2mu_datas3,3,"#bf{0.8<|y_{#mu#mu}|<1.2}");
+            afbhist(df_2mu_MC3, df_2mu_datas3, 3, "#bf{0.8<|y_{#mu#mu}|<1.2}");
             pad4->cd();
-            afbhist(df_2mu_MC4,df_2mu_datas4,4,"#bf{1.2<|y_{#mu#mu}|<1.6}");
+            afbhist(df_2mu_MC4, df_2mu_datas4, 4, "#bf{1.2<|y_{#mu#mu}|<1.6}");
             pad5->cd();
-            afbhist(df_2mu_MC5,df_2mu_datas5,5,"#bf{1.6<|y_{#mu#mu}|<2.0}");
+            afbhist(df_2mu_MC5, df_2mu_datas5, 5, "#bf{1.6<|y_{#mu#mu}|<2.0}");
             pad6->cd();
-            afbhist(df_2mu_MC6,df_2mu_datas6,6,"#bf{2.0<|y_{#mu#mu}|<2.4}");
+            afbhist(df_2mu_MC6, df_2mu_datas6, 6, "#bf{2.0<|y_{#mu#mu}|<2.4}");
 
             //saving histogram
             save_histogram(c, "afb", "afb");
 
         }else{
-            std::cerr << "Your file haven't the right columns"<<std::endl;
+            std::cerr << "Your file haven't the right columns" << std::endl;
         }
     }else{
-        std::cerr << "Your files don't exist, change datas!"<<std::endl;
+        std::cerr << "Your files don't exist, change datas!" << std::endl;
     }
 }
--- src/costheta_hist.cpp
+++ src/costheta_hist.cpp
@@ -19,37 +19,37 @@
 void costheta_hist(std::string filepath_MC, std::string filepath_datas){
 
     //check if the path is correct
-    if(TFile::Open(filepath_datas.c_str())!=nullptr && TFile::Open(filepath_MC.c_str())!=nullptr){
+    if(TFile::Open(filepath_datas.c_str()) != nullptr && TFile::Open(filepath_MC.c_str()) != nullptr){
 
         //creating dataframes
         ROOT::RDataFrame df_MC("Events", filepath_MC);
         ROOT::RDataFrame df_datas("Events", filepath_datas);
 
         //checking if they have the right column
-        if( df_MC.HasColumn("Muon_pt") && df_MC.HasColumn("Muon_mass") && df_MC.HasColumn("Muon_phi") && df_MC.HasColumn("Muon_eta") && df_datas.HasColumn("Muon_pt") && df_datas.HasColumn("Muon_mass") && df_datas.HasColumn("Muon_phi")  && df_datas.HasColumn("Muon_eta") ){
+        if(df_MC.HasColumn("Muon_pt") && df_MC.HasColumn("Muon_mass") && df_MC.HasColumn("Muon_phi") && df_MC.HasColumn("Muon_eta") && df_datas.HasColumn("Muon_pt") && df_datas.HasColumn("Muon_mass") && df_datas.HasColumn("Muon_phi")  && df_datas.HasColumn("Muon_eta") ){
         
             //defining all the necessary quantities
-            auto df_2mu_MC= allquantities(df_MC);
-            auto df_2mu_datas= allquantities(df_datas);
+            auto df_2mu_MC = allquantities(df_MC);
+            auto df_2mu_datas = allquantities(df_datas);
     
             //filter the dataframes
-            auto df_2mu_MCy1= df_2mu_MC.Filter("fabs(y)<0.4 ", "y1");
-            auto df_2mu_MCy2= df_2mu_MC.Filter("fabs(y)<1.2 && fabs(y)>0.8", "y2");
-            auto df_2mu_MCy3= df_2mu_MC.Filter("fabs(y)<2.0 && fabs(y)>1.6", "y3");
+            auto df_2mu_MCy1 = df_2mu_MC.Filter("fabs(y)<0.4 ", "y1");
+            auto df_2mu_MCy2 = df_2mu_MC.Filter("fabs(y)<1.2 && fabs(y)>0.8", "y2");
+            auto df_2mu_MCy3 = df_2mu_MC.Filter("fabs(y)<2.0 && fabs(y)>1.6", "y3");
 
-            auto df_2mu_datasy1= df_2mu_datas.Filter("fabs(y)<0.4 ", "y1");
-            auto df_2mu_datasy2= df_2mu_datas.Filter("fabs(y)<1.2 && fabs(y)>0.8", "y2");
-            auto df_2mu_datasy3= df_2mu_datas.Filter("fabs(y)<2.0 && fabs(y)>1.6", "y3");
+            auto df_2mu_datasy1 = df_2mu_datas.Filter("fabs(y)<0.4 ", "y1");
+            auto df_2mu_datasy2 = df_2mu_datas.Filter("fabs(y)<1.2 && fabs(y)>0.8", "y2");
+            auto df_2mu_datasy3 = df_2mu_datas.Filter("fabs(y)<2.0 && fabs(y)>1.6", "y3");
     
             //making histograms
-            coshisto(df_2mu_MCy1,df_2mu_datasy1, "costheta1","#bf{0.0<|y_{#mu#mu}|<0.4}",0.45,0.80,0.65,0.86,"c1");
-            coshisto(df_2mu_MCy2,df_2mu_datasy2, "costheta2","#bf{0.8<|y_{#mu#mu}|<1.2}",0.15,0.82,0.35,0.88,"c2");
-            coshisto(df_2mu_MCy3,df_2mu_datasy3, "costheta3","#bf{1.6<|y_{#mu#mu}|<2.0}",0.70,0.80,0.90,0.86,"c3");
+            coshisto(df_2mu_MCy1, df_2mu_datasy1, "costheta1", "#bf{0.0<|y_{#mu#mu}|<0.4}", 0.45, 0.80, 0.65, 0.86, "c1");
+            coshisto(df_2mu_MCy2, df_2mu_datasy2, "costheta2", "#bf{0.8<|y_{#mu#mu}|<1.2}", 0.15, 0.82, 0.35, 0.88, "c2");
+            coshisto(df_2mu_MCy3, df_2mu_datasy3, "costheta3", "#bf{1.6<|y_{#mu#mu}|<2.0}", 0.70, 0.80, 0.90, 0.86, "c3");
 
         }else{
-            std::cerr << "Your file haven't the right columns"<<std::endl;
+            std::cerr << "Your file haven't the right columns" << std::endl;
         }
     }else{
-        std::cerr << "Your files don't exist, change datas!"<<std::endl;
+        std::cerr << "Your files don't exist, change datas!" << std::endl;
     }
 }
