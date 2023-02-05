a56013bde1ce6af6069c15ecbb88cd6ab39506c4
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-02-05 17:12:39 +0100

    Add files via upload

--- src/filterDf.cpp
+++ src/filterDf.cpp
@@ -26,7 +26,7 @@ int filterDf(std::string filepath_MC, std::string filepath_datas, std::string fi
             df1_MC = df1_MC.Filter("(Muon_pt[0] > 25 && Muon_pt[1] > 15)|| (Muon_pt[1] > 25 && Muon_pt[0] > 15)","Selection on muon pt");
             df1_MC = df1_MC.Filter("Muon_pfRelIso03_chg[0] < 0.1*Muon_pt[0] && Muon_pfRelIso03_chg[1] < 0.1*Muon_pt[1] ","Selection on muon isolation");
             df1_MC = df1_MC.Filter("Muon_dxy[0] < 0.2 && Muon_dxy[1] < 0.2","Selection on transverse distance");
-
+            
             auto df1_datas = df_datas.Filter("nMuon == 2","Events with only two muons");
             df1_datas = df1_datas.Filter("Muon_charge[0]*Muon_charge[1] == -1","Muons with opposite charge");
             df1_datas = df1_datas.Filter("fabs(Muon_eta[0]) < 2.4 && fabs(Muon_eta[1]) < 2.4","Selection on muon eta");
